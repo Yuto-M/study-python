69fa9ed9e76053f0e095cf434c19f7a1c6883a42
宮川優斗 <miyakaway7@gmail.com>
2020-12-12 21:15:26 +0900

    関数とかifの練習でgreet.py作ってみた

--- greet.py
+++ greet.py
@@ -1,2 +1,11 @@
-
 def greet(hour):
+    if 4 < hour < 12:
+        print('good morning')
+    elif 12 < hour < 18:
+        print('good afternoon')
+    else:
+        print('good evening')
+
+greet(8)
+greet(17)
+greet(21)
